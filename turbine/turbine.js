ff838d865fa4a2262e5b6f61aece892b34ff221f
John McLear <john@mclear.co.uk>
2013-05-22 15:27:59 +0000

    bring in working python

--- turbine/turbine.js
+++ turbine/turbine.js
@@ -3,9 +3,8 @@ Edit Settings.js, providing your email settings etc.
 Set this up as a cron job to run every 5 minutes.
 You need Internet access, an ADC board, a Pi, A wind Turbine, A way to get a 5v signal onto the ADC from the turbine, I use a Car USB charger... 
 */
-var request = require("request"), async = require("async"), email = require("emailjs"), sys = require('sys'), exec = require('child_process').exec, settings = require("./settings.js");
+var request = require("request"), async = require("async"), email = require("emailjs"), sys = require('sys'), settings = require("./settings.js"), exec = require('child_process').exec;
 var metOfficeUrl = "http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/"+settings.metOfficeLocation+"?res=hourly&key="+settings.metOfficeKey;
-
 console.log("Est wind speed turbine should spin at: " +settings.estWindSpeedOfWindy + "mph");
 async.waterfall([
   
@@ -15,59 +14,55 @@ async.waterfall([
     var mins = date.getHours() * 60;
     request(metOfficeUrl, function (error, response, body) { // HTTP API Request
       if (!error && response.statusCode == 200) {
-  	    body = JSON.parse(body); // Response
-	    var times = body.SiteRep.DV.Location.Period.Rep;
-	    for (var i = 0; i < times.length; i++) {
-  	      if(times[i]["$"] == mins){ // If it's the current hour
+        body = JSON.parse(body); // Response
+        var times = body.SiteRep.DV.Location.Period[0].Rep; // keep an eye on this..
+        for (var i = 0; i < times.length; i++) {
+          if(times[i]["$"] == mins){ // If it's the current hour
             console.log("Wind speed outside according to met office is:", times[i]["W"]);
-			console.log("Do We expect the turbine to be spinning?", times[i]["W"] >= settings.estWindSpeedOfWindy);
-			if(times[i]["W"] >= settings.estWindSpeedOfWindy){
-			  callback(null, true); // calls back if its windy or not
-			}else{
-			  console.log("It is not even windy enough to care");
-			  callback(null, false); // ist not even windy enough to care
-			}
-		  }
+            console.log("Do We expect the turbine to be spinning?", times[i]["W"] >= settings.estWindSpeedOfWindy);
+            if(times[i]["W"] >= settings.estWindSpeedOfWindy){
+	      callback(null, true); // calls back if its windy or not
+  	    }else{
+	      console.log("It is not even windy enough to care");
+	      callback(null, false); // ist not even windy enough to care
+    	    }
+	  }
         } 
       }
-	  if(error){
-	    callback("HTTP Request Error", error);
-	  }
+      if(error){
+        callback("HTTP Request Error", error);
+      }
     });
   },
   
   /* Is the Pi recieving a message from the ADC Board that we have a successful power read */
   function isItWindyAccordingToTurbine(shouldBeWindy, callback){
     if(shouldBeWindy){
-  	  // http://nodejs.org/api.html#_child_processes
-      exec("python adc.py", function (error, stdout, stderr) { 
-        // sys.print('stdout: ' + stdout); sys.print('stderr: ' + stderr); 
-        if (error !== null) {
-          // console.log('exec error: ' + error); 
+       // http://nodejs.org/api.html#_child_processes
+      exec("python turbineGenerating.py 0 0x68", function (error, stdout, stderr) { 
+        if (error === null) { 
+          console.log("The turbine is generating power, yay :)"); // it is, so no drama :)
+          callback(null, true);
         }else{
-		  if(stdout >= 5){
-  	        console.log("The turbine is generating power, yay :)"); // it is, so no drama :)
-	        callback(null, true);
-	      }else{
-  	        console.log("The turbine should be generating power but isn't...."); // it isn't so let the owner know
-  	        callback(null, false);
-	      }
-		}
-	  });
-	}else{
-	   callback(null, true); // it shouldnt be windy anyway..
-	}
+          console.log("The turbine should be generating power but isn't...."); // it isn't so let the owner know
+          callback(null, false);
+        }
+      });
+    }else{
+      callback(null, true); // it shouldnt be windy anyway..
+    }
   },
   
   /* Send an Email to me to let me know something is b0rked */
   function sendEmail(isWindy, callback){
     if(!isWindy){
-      server.send(message, function(err, message) { console.log(err || message); 
-  	    if(err){
-	      callback("Email error sending", err);
-		}
-	  });
-	}
+      server.send(message, function(err, message) { 
+        console.log(err || message); 
+        if(err){
+          callback("Email error sending", err);
+        }
+      });
+    }
   }
 ], function(err, result){
   if(err){
---
+++ turbine/turbineGenerating.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# read abelectronics Delta Sigma board inputs and return exit code 0 if the turbine is generating IE if 5v exists on channel 0
+# # Requries Python 2.7
+# Requires SMBus 
+# I2C API depends on I2C support in the kernel
+
+# Version 1.0  - 29/04/2013
+# Version History:
+# 1.0 - Initial Release
+
+#
+# Usage: changechannel(address, hexvalue) to change to new channel on adc chips
+# Usage: getadcreading(address, hexvalue) to return value in volts from selected channel.
+#
+# address = adc_address1 or adc_address2 - Hex address of I2C chips as configured by board header pins.
+
+from smbus import SMBus
+import re, sys
+
+adc_address1 = 0x68
+adc_address2 = 0x69
+
+# create byte array and fill with initial values to define size
+adcreading = bytearray()
+
+adcreading.append(0x00)
+adcreading.append(0x00)
+adcreading.append(0x00)
+adcreading.append(0x00)
+
+varDivisior = 64 # from pdf sheet on adc addresses and config
+
+# detect i2C port number and assign to i2c_bus
+for line in open('/proc/cpuinfo').readlines():
+    m = re.match('(.*?)\s*:\s*(.*)', line)
+    if m:
+        (name, value) = (m.group(1), m.group(2))
+        if name == "Revision":
+            if value [-4:] in ('0002', '0003'):
+                i2c_bus = 0
+            else:
+                i2c_bus = 1
+            break
+               
+
+bus = SMBus(i2c_bus)
+ 
+def changechannel(address, adcConfig):
+	tmp= bus.write_byte(address, adcConfig)
+
+def getadcreading(address, adcConfig):
+	adcreading = bus.read_i2c_block_data(address,adcConfig)
+	h = adcreading[0]
+	m = adcreading[1]
+	l = adcreading[2]
+	s = adcreading[3]
+	# wait for new data
+	while (s & 128):
+		adcreading = bus.read_i2c_block_data(address,adcConfig)
+		h = adcreading[0]
+		m = adcreading[1]
+		l = adcreading[2]
+		s = adcreading[3]
+	
+	# shift bits to product result
+	t = ((h & 0b00000001) << 16) | (m << 8) | l
+	# check if positive or negative number and invert if needed
+	if (h > 128):
+		t = ~(0x020000 - t)
+	return (t/varDivisior)
+
+	
+while True:
+	changechannel(adc_address1, 0x9C)
+	output = getadcreading(adc_address1,0x9C);
+	if output > 2000:
+		print("Generating");
+		sys.exit(0);
+	else:
+		print (output);
+		print ("Channel 1: %02f" % getadcreading(adc_address1,0x9C))
+		sys.exit(1);
