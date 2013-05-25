3f4954f52d824775bf3f1bfcfb902e6edd35c51b
John McLear <john@mclear.co.uk>
2013-05-25 13:57:02 +0000

    hah, funny, getting speed was all wrong, now massively fixed

--- turbine/turbine.js
+++ turbine/turbine.js
@@ -4,30 +4,26 @@ Set this up as a cron job to run every 5 minutes.
 You need Internet access, an ADC board, a Pi, A wind Turbine, A way to get a 5v signal onto the ADC from the turbine, I use a Car USB charger... 
 */
 var request = require("request"), async = require("async"), email = require("emailjs"), sys = require('sys'), settings = require("./settings.js"), exec = require('child_process').exec;
-var metOfficeUrl = "http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/"+settings.metOfficeLocation+"?res=hourly&key="+settings.metOfficeKey;
-console.log(metOfficeUrl);
+var date = new Date();
+var time = date.toISOString();
+var metOfficeUrl = "http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/"+settings.metOfficeLocation+"?res=hourly&key="+settings.metOfficeKey + "&time="+time;
+
+// console.log(metOfficeUrl);
 console.log("Est wind speed turbine should spin at: " +settings.estWindSpeedOfWindy + "mph");
 async.waterfall([
   
   /* Should it be windy?  We make a call to the met office and ask them */
   function shouldItBeWindy(callback){
-    var date = new Date();
-    var mins = date.getHours() * 60;
     request(metOfficeUrl, function (error, response, body) { // HTTP API Request
       if (!error && response.statusCode == 200) {
         body = JSON.parse(body); // Response
-        var times = body.SiteRep.DV.Location.Period[0].Rep; // keep an eye on this..
-        for (var i = 0; i < times.length; i++) {
-          if(times[i]["$"] == mins){ // If it's the current hour
-            console.log("Wind speed outside according to met office is:", times[i]["S"]);
-            console.log("Do We expect the turbine to be spinning?", times[i]["S"] >= settings.estWindSpeedOfWindy);
-            if(times[i]["S"] >= settings.estWindSpeedOfWindy){
-	      callback(null, true); // calls back if its windy or not
-  	    }else{
-	      console.log("It is not even windy enough to care");
-	      callback(null, false); // ist not even windy enough to care
-    	    }
-	  }
+        if ( body.SiteRep.DV.Location.Period.Rep.S >= settings.estWindSpeedOfWindy){
+          console.log("Wind speed outside according to met office is:", times[i]["S"]);
+          console.log("Do We expect the turbine to be spinning?", times[i]["S"] >= settings.estWindSpeedOfWindy);
+          callback(null, true); // calls back if its windy or not
+        }else{
+          console.log("It is not even windy enough to care");
+          callback(null, false); // ist not even windy enough to care
         } 
       }
       if(error){
@@ -39,7 +35,6 @@ async.waterfall([
   /* Is the Pi recieving a message from the ADC Board that we have a successful power read */
   function isItWindyAccordingToTurbine(shouldBeWindy, callback){
     if(shouldBeWindy){
-       // http://nodejs.org/api.html#_child_processes
       exec("python turbineGenerating.py 0 0x68", function (error, stdout, stderr) { 
         if (error === null) { 
           console.log("The turbine is generating power, yay :)"); // it is, so no drama :)
